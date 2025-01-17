82a6803b12dff980a482ac7ea10d0ad04278f4f1
henrietteharmse <henrietteharmse@gmail.com>
2025-01-17 09:58:24 +0000

    Removed frontend

--- oxo2-frontend/src/app/mui.ts
+++
@@ -1,9 +0,0 @@
-import { createTheme } from "@mui/material";
-
-export const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#d4522c",
-    },
-  },
-});
--- oxo2-frontend/src/pages/documentation/index.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Documentation() {
-  return (
-    <main className="container mx-auto">
-      <div className="text-2xl font-bold my-6">OxO Documentation</div>
-    </main>
-  );
-}
