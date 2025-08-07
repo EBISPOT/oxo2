c945b34621cb782172a86e4b45049ee1ef6c7461
henrietteharmse <henrietteharmse@gmail.com>
2025-08-07 10:31:30 +0100

    Deleted files no longer used.

--- oxo2-dataload/dockerComposeBuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export OXO_CONFIG=./../oxo-config.json
-export JAVA_OPTS="-Xms5G -Xmx10G"
-
-docker compose --progress plain build
--- oxo2-dataload/dockerComposeUp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export USER_ID=$(id -u)
-export GROUP_ID=$(id -g)
-
-export OXO_CONFIG=./../oxo-config.json
-export JAVA_OPTS="-Xms5G -Xmx10G"
-
-docker compose --verbose up
